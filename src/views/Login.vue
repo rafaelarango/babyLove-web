a61f0eb4d045809a4e6b843afc8675d45f2a7128
rafaelarango <arangoramirezrafael@gmail.com>
2021-07-16 23:59:30 -0500

    Diseño de login y ok en confirmando usuario

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,37 +2,35 @@
   <div class="container w-75 bg-primary mt-5 rounded shadow">
     <div class="row align-items-stretch">
       <div class="col bg d-none d-lg-block col-md-5 col-lg-5 col-xl-6 rounded"></div>
-      <div class="col-6 bg-white p-5 rounded-end">
+      <div class="col-6 bg-white p-4 rounded-end">
         <div class="text-end"></div>
-        <img src="" width="48" alt="logo" />
-        <h2 class="fw-bold text-center py-5">Bienvenido</h2>
-
+        <img  class="py-3" src="../../public\assets\img\logo.png" width="140" alt="logo" />
         <form action="#">
-          <div class="mb-4">
+          <div class="mb-3">
             <label for="email" class="form-label">Correo electronico</label>
-            <input type="email" class="form-control" name="email" />
+            <input type="email" class="form-control" name="email" v-model="email" />
           </div>
           <div class="mb-4">
-            <label for="password" class="form-label">Correo electronico</label>
-            <input type="password" class="form-control" name="password" />
+            <label for="password" class="form-label">Contraseña</label>
+            <input type="password" class="form-control" name="password" v-model="password" />
           </div>
           <div class="mb-4 form-check">
-            <input type="checkbox" name="connectd" class="form-check-input" />
+            <input type="checkbox" name="connected" class="form-check-input" />
             <label for="connected" class="form-check-label"
               >Mantenerme conectado</label
             >
           </div>
           <div class="d-grid">
-            <button type="submit" class="btn btn-primary">
+            <button type="submit" class="btn btn-primary" @click="login">
               Iniciar sesion
             </button>
           </div>
           <div class="my-3">
             <span>No tienes cuenta? <a href="#">Registrate</a></span> <br />
-            <span> <a href="#">Recuperar password</a></span>
+            <span> <a href="#">Recuperar contraseña</a></span>
           </div>
         </form>
-        <div class="container w-100 my-5">
+        <div class="container w-100 ">
           <div class="row text-center">
             <div class="col-12">Iniciar sesion</div>
           </div>
@@ -41,7 +39,7 @@
               <button class="btn btn-outline-primary w-100 my-1">
                 <div class="row align-items-center">
                   <div class="col-2 d-none d-md-block">
-                    <img src="" width="32" alt="face" />
+                    <img src="../../public\assets\img\face.png" width="32" alt="face" />
                   </div>
                   <div class="col-12 col-md-10 text-center">
                     Facebook
@@ -52,8 +50,8 @@
             <div class="col">
               <button class="btn btn-outline-danger w-100 my-1">
                 <div class="row align-items-center">
-                  <div class="clo-2 d-none d-md-block">
-                    <img src="" width="32" alt="face" />
+                  <div class="col-2 d-none d-md-block">
+                    <img src="../../public\assets\img\google.png" width="32" alt="Google" />
                   </div>
                   <div class="col-12 col-md-10 text-center">
                     Google
@@ -83,8 +81,8 @@ export default {
   name: "Login",
   data() {
     return {
-      email: "arango",
-      password: "123",
+      email: "arangoramirezrafael@gmail.com",
+      password: "rafarafarafa",
       // getData: {}
       error: false,
       errorMessage: "",
@@ -136,7 +134,7 @@ export default {
     background: linear-gradient(to right, #ffa751, #ffe259)
   }
   .bg{
-    background-image: url();
+    background-image: url(); 
     background-position: center center;
   }
 </style>
