5a759aedf17d60bf70b826a0412185486c302176
rafaelarango <arangoramirezrafael@gmail.com>
2021-07-16 21:49:37 -0500

    Login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,9 +3,9 @@
     <h1>Login</h1>
     <input type="text" v-model="email" />
     <input type="text" v-model="password" />
-    <button @click="login">Sing in</button>
+    <button @click="login">Sign in</button>
     <router-link to="/register">Sign up</router-link>
-    <p>{{getData}}</p>
+    <!-- <p>{{getData}}</p> -->
   </div>
 </template>
 
@@ -16,20 +16,51 @@ export default {
   name: "Login",
   data() {
     return {
-      email: "",
-      password: "",
-      getData: {}
+      email: "arango",
+      password: "123",
+      // getData: {}
+      error: false,
+      errorMessage: ""
     };
   },
   methods: {
    async login() {
       //console.log(`Email: ${this.email}, Password: ${this.password}`);
       
-      const dataGet =  await axios.get('https://jsonplaceholder.typicode.com/todos/1');
-      console.log(dataGet);
-      this.getData = dataGet.data;
+      // const dataGet =  await axios.get('https://jsonplaceholder.typicode.com/todos/1');
+      // console.log(dataGet);
+      // this.getData = dataGet.data;
+
+      try {
+
+        const user = {
+          email: this.email,
+          password: this.password,
+        };
+        console.log("Vamos a imprimir el usuario", user);
+
+        const userLogin = await axios.post("http://localhost:3000/api/users/login", user);
+        console.log("Optenemos usuari con axios" + JSON.stringify(userLogin.data))  // POR QUE NOS IMPRIME OBJETO OBJETO
+
+        this.error = false;
+        this.errorMessage = "";
+        localStorage.setItem("userApp", JSON.stringify(userLogin.data));
+
+        this.$router.push({
+          name: "Home",
+        });
+
+        
+      } catch (error) {
+        this.error = true;
+        this.errorMessage = error.response.data.message; // como aceder al mensage
+        console.error(error);
+        console.log(error.response.data.message);
+      }
+      
     },
   },
 };
 </script>
 <style></style>
+
